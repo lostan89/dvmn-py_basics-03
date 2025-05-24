7a0ed96b39e215d5874a6b1bf0c63f6359ea6ffc
lostan89 <skeeperenator@gmail.com>
2025-05-24 19:19:10 +0300

    Update main.py
    добавлены новые переменные sender и recipient

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ friend_name = 'Петруша'
 
 my_name = 'Иваныч'
 
+sender = 'ewankarbasov@yandex.ru'
+
+recipient = 'superkeeper@mail.ru'
+
 letter = '''\
-From: ewankarbasov@yandex.ru
-To: superkeeper@mail.ru
+From: {sender}
+To: {recipient}
 Subject: Devman3
 Content-Type: text/plain; charset="UTF-8";
 
@@ -33,11 +37,11 @@ Content-Type: text/plain; charset="UTF-8";
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → {website}  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''.format(friend_name=friend_name, my_name=my_name, website=website)
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''.format(sender = sender, recipient = recipient, friend_name=friend_name, my_name=my_name, website=website)
 
 letter = letter.encode("UTF-8")
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
 server.login(os.environ['LOGIN'], os.environ['TOKEN'])
-server.sendmail('ewankarbasov@yandex.ru', 'superkeeper@mail.ru', letter)
+server.sendmail(sender, recipient, letter)
 server.quit()
